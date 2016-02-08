8a37d4176b723af10a0d360e95e0bb94a6ab1164
Rupert Nash <rupert.nash@ed.ac.uk>
2016-02-08 23:08:21 +0000

    move swimmers and tracers over to target

--- lbswim/liblbswim/SwimmerArray.cu
+++ lbswim/liblbswim/SwimmerArray.cu
@@ -5,14 +5,15 @@
 #include "delta.cu"
 #include "interp.cu"
 
-__global__ void DoInitPrng(const unsigned long long seed,
+__targetEntry__ void DoInitPrng(const unsigned long long seed,
 			   RandList& prngs) {
-  const size_t n = prngs.indexer.shape[0];
-  const size_t i = threadIdx.x + blockIdx.x * blockDim.x;
-  /* If we're out of range, skip */
-  if (i >= n) return;
-  auto this_prng = prngs[i][0];
-  curand_init(seed, i, 0, &this_prng);
+  auto n = prngs.Shape();
+  FOR_TLP(n) {
+    FOR_ILP(i) {
+      auto this_prng = prngs[i][0];
+      curand_init(seed, i[0], 0, &this_prng);
+    }
+  }
 }
 
 SwimmerArray::SwimmerArray(const size_t num_, const CommonParams* p) : 
@@ -20,13 +21,12 @@ SwimmerArray::SwimmerArray(const size_t num_, const CommonParams* p) :
   r(array<size_t,1>{num_}), v(array<size_t,1>{num_}), n(array<size_t,1>{num_}),
   prng(array<size_t,1>{num})
 {
-  const size_t numBlocks = (num + BlockSize - 1)/BlockSize;
-  DoInitPrng<<<numBlocks, BlockSize>>>(p->seed, prng.Device());
+  targetLaunch(DoInitPrng, prng.Host().Shape())(p->seed, prng.Device());
   // pull state to host
   prng.D2H();
 }
 
-__device__ double atomicAdd(double* address, double val) {
+__target__ double atomicAdd(double* address, double val) {
   unsigned long long int* address_as_ull = (unsigned long long int*)address;
   unsigned long long int old = *address_as_ull, assumed;
   do {
@@ -41,7 +41,7 @@ __device__ double atomicAdd(double* address, double val) {
   return __longlong_as_double(old);
 }
 
-__device__ void AccumulateDeltaForce(VectorField& lat_force, const double* r, const double* F) {
+__target__ void AccumulateDeltaForce(VectorField& lat_force, const double* r, const double* F) {
   size_t indices[DQ_d][4];
   double deltas[DQ_d][4];
   double x, x0;
@@ -83,130 +83,128 @@ __global__ void DoSwimmerArrayAddForces(const CommonParams& p,
 					const VectorList& swim_r,
 					const VectorList& swim_n,
 					VectorField& lat_force) {
-  const size_t nSwim = swim_r.indexer.shape[0];
-  const size_t iSwim = threadIdx.x + blockIdx.x * blockDim.x;
-  /* If we're out of range, skip */
-  if (iSwim >= nSwim) return;
+  auto nSwim = swim_r.Shape();
+  FOR_TLP(nSwim) {
+    FOR_ILP(iSwim) {  
+      size_t a;
+      /* tail end */
+      /* only force is the propulsion */
+      double r[DQ_d];
+      double force[DQ_d];
   
-  size_t a;
-  /* tail end */
-  /* only force is the propulsion */
-  double r[DQ_d];
-  double force[DQ_d];
+      for (a=0; a<DQ_d; a++) {
+	r[a] = swim_r[iSwim][a] - swim_n[iSwim][a] * p.l;
+	force[a] = -p.P * swim_n[iSwim][a];
+      }
+      AccumulateDeltaForce(lat_force, r, force);
   
-  for (a=0; a<DQ_d; a++) {
-    r[a] = swim_r[iSwim][a] - swim_n[iSwim][a] * p.l;
-    force[a] = -p.P * swim_n[iSwim][a];
+      /* head end */
+      /* opposite force */
+      for (a=0; a<DQ_d; a++) {
+	r[a] = swim_r[iSwim][a];
+	force[a] *= -1.0;
+      }
+  
+      AccumulateDeltaForce(lat_force, r, force);
+    }
   }
-  AccumulateDeltaForce(lat_force, r, force);
-  
-  /* head end */
-  /* opposite force */
-  for (a=0; a<DQ_d; a++) {
-    r[a] = swim_r[iSwim][a];
-    force[a] *= -1.0;
-  }
-  
-  AccumulateDeltaForce(lat_force, r, force);
 }
 
 void SwimmerArray::AddForces(Lattice* lat) const {
-  const int numBlocks = (num + BlockSize - 1)/BlockSize;
-
-  DoSwimmerArrayAddForces<<<numBlocks, BlockSize>>>(common.Device(),
-						    r.Device(), n.Device(),
-						    lat->data.force.Device());
+  targetLaunch(DoSwimmerArrayAddForces, r.Host().Shape())(common.Device(),
+							  r.Device(), n.Device(),
+							  lat->data.force.Device());
+  targetSynchronize();
 }
 
-__global__ void DoSwimmerArrayMove(const CommonParams& common,
-				   VectorList& swim_r,
-				   VectorList& swim_v,
-				   VectorList& swim_n,
-				   RandList& swim_prng,
-				   const VectorField& lat_u) {
+__targetEntry__ void DoSwimmerArrayMove(const CommonParams& common,
+					VectorList& swim_r,
+					VectorList& swim_v,
+					VectorList& swim_n,
+					RandList& swim_prng,
+					const VectorField& lat_u) {
   /* Updates the swimmers' positions using:
    *     Rdot = v(R) + Fn_/(6 pi eta a)
    * where v(R) is the interpolated velocity at the position of the
    * swimmer, a is the radius and n_ is the orientation.
    */  
-  const size_t nSwim = swim_r.indexer.shape[0];
+  auto shape = swim_r.Shape();
+  FOR_TLP(shape) {
+    FOR_ILP(iSwim) {
   
-  const size_t iSwim = threadIdx.x + blockIdx.x * blockDim.x;
-  /* If we're out of range, skip */
-  if (iSwim >= nSwim) return;
-  
-  auto size = lat_u.indexer.shape;
+      auto size = lat_u.Shape();
 
-  auto v = InterpVelocity(lat_u, swim_r[iSwim]);
+      auto v = InterpVelocity(lat_u, swim_r[iSwim]);
   
-  double rDot[DQ_d];
-  double rMinus[DQ_d];
+      double rDot[DQ_d];
+      double rMinus[DQ_d];
   
-  for (int d=0; d<DQ_d; d++) {
-    rDot[d] = common.mobility * common.P * swim_n[iSwim][d];
+      for (int d=0; d<DQ_d; d++) {
+	rDot[d] = common.mobility * common.P * swim_n[iSwim][d];
 
-    if (!common.translational_advection_off)
-      rDot[d] += v[d];
+	if (!common.translational_advection_off)
+	  rDot[d] += v[d];
     
-    rMinus[d] = swim_r[iSwim][d] - swim_n[iSwim][d] * common.l;
-  }
+	rMinus[d] = swim_r[iSwim][d] - swim_n[iSwim][d] * common.l;
+      }
   
-  /* self.applyMove(lattice, rDot) */
-  for (int d=0; d<DQ_d; d++) {
-    swim_v[iSwim][d] = rDot[d];
+      /* self.applyMove(lattice, rDot) */
+      for (int d=0; d<DQ_d; d++) {
+	swim_v[iSwim][d] = rDot[d];
     
-    /* new position */
-    swim_r[iSwim][d] += rDot[d];
-    /* deal with PBC */
-    swim_r[iSwim][d] = fmod(swim_r[iSwim][d] + size[d], double(size[d]));
-  }
+	/* new position */
+	swim_r[iSwim][d] += rDot[d];
+	/* deal with PBC */
+	swim_r[iSwim][d] = fmod(swim_r[iSwim][d] + size[d], double(size[d]));
+      }
   
-  double newn[DQ_d];
-  double norm;
+      double newn[DQ_d];
+      double norm;
   
-  float rand = curand_uniform(&swim_prng[iSwim][0]);
-  if (rand < common.alpha) {
-    // Tumble - i.e. pick a random unit vector
-    // Pick a point from a Gaussian distribution and normalise it
-    // We'll do the norm below.
-    for (int d=0; d<DQ_d; d++) {
-      newn[d] = curand_normal_double(&swim_prng[iSwim][0]);
-      norm += newn[d]*newn[d];
+      float rand = curand_uniform(&swim_prng[iSwim][0]);
+      if (rand < common.alpha) {
+	// Tumble - i.e. pick a random unit vector
+	// Pick a point from a Gaussian distribution and normalise it
+	// We'll do the norm below.
+	for (int d=0; d<DQ_d; d++) {
+	  newn[d] = curand_normal_double(&swim_prng[iSwim][0]);
+	  norm += newn[d]*newn[d];
+	}
+
+      } else if (!common.rotational_advection_off){
+	// Normal rotation
+	auto vMinus = InterpVelocity(lat_u, rMinus);
+
+	double nDot[DQ_d];
+	for (int d=0; d<DQ_d; d++) {
+	  nDot[d] = v[d] - vMinus[d];
+	  /* now nDot = v(rPlus) - v(rMinus) */
+	  /* so divide by l to get the rate */
+	  nDot[d] /= common.l;
+	}
+    
+	/* self.applyTurn(lattice, nDot) */
+    
+	for (int d=0; d<DQ_d; d++) {
+	  newn[d] =  swim_n[iSwim][d] + nDot[d];
+	  norm += newn[d]*newn[d];
+	}
+      } else {
+	// Do nothing if rotational advection is off
+      }
+
+      norm = sqrt(norm);
+      for (int d=0; d<DQ_d; d++) {
+	swim_n[iSwim][d] = newn[d] / norm;
+      }
     }
-
-  } else if (!common.rotational_advection_off){
-    // Normal rotation
-    auto vMinus = InterpVelocity(lat_u, rMinus);
-
-    double nDot[DQ_d];
-    for (int d=0; d<DQ_d; d++) {
-      nDot[d] = v[d] - vMinus[d];
-      /* now nDot = v(rPlus) - v(rMinus) */
-      /* so divide by l to get the rate */
-      nDot[d] /= common.l;
-    }
-    
-    /* self.applyTurn(lattice, nDot) */
-    
-    for (int d=0; d<DQ_d; d++) {
-      newn[d] =  swim_n[iSwim][d] + nDot[d];
-      norm += newn[d]*newn[d];
-    }
-  } else {
-    // Do nothing if rotational advection is off
-  }
-
-  norm = sqrt(norm);
-  for (int d=0; d<DQ_d; d++) {
-    swim_n[iSwim][d] = newn[d] / norm;
   }
 }
 
 void SwimmerArray::Move(Lattice* lat) {
-  const int numBlocks = (num + BlockSize - 1)/BlockSize;
-  
-  DoSwimmerArrayMove<<<numBlocks, BlockSize>>>(common.Device(),
-					       r.Device(), v.Device(), n.Device(),
-					       prng.Device(),
-					       lat->data.u.Device());
+  targetLaunch(DoSwimmerArrayMove, r.Host().Shape())(common.Device(),
+						     r.Device(), v.Device(), n.Device(),
+						     prng.Device(),
+						     lat->data.u.Device());
+  targetSynchronize();
 }
--- lbswim/liblbswim/TracerArray.cu
+++ lbswim/liblbswim/TracerArray.cu
@@ -10,36 +10,33 @@ TracerArray::TracerArray(const size_t num_) :
 }
 
 
-__global__ void DoTracerArrayMove(VectorList& part_r,
-				  VectorList& part_s,
-				  VectorList& part_v,
-				  const VectorField& lat_u) {
-  const size_t nPart = part_r.indexer.shape[0];
-  const size_t iPart = threadIdx.x + blockIdx.x * blockDim.x;
-  /* If we're out of range, skip */
-  if (iPart >= nPart) return;
-  
-  /* Updates the positions using:
-   *     Rdot = v(R) 
-   * where v(R) is the interpolated velocity at the position of the
-   * particle
-   */  
-  auto size = lat_u.indexer.shape;
-  auto fluid_v = InterpVelocity(lat_u, part_r[iPart]);
-    
-  for (int d=0; d<DQ_d; d++) {
-    part_v[iPart][d] = fluid_v[d];
-    part_r[iPart][d] += fluid_v[d];
-    /* deal with PBC */
-    part_r[iPart][d] = fmod(part_r[iPart][d] + size[d], double(size[d]));
-    /* and then update the unwrapped coords */
-    part_s[iPart][d] += fluid_v[d];
+__targetEntry__ void DoTracerArrayMove(VectorList& part_r,
+				       VectorList& part_s,
+				       VectorList& part_v,
+				       const VectorField& lat_u) {
+  FOR_TLP(part_r.Shape()) {
+    FOR_ILP(iPart) {
+      /* Updates the positions using:
+       *     Rdot = v(R) 
+       * where v(R) is the interpolated velocity at the position of the
+       * particle
+       */  
+      auto size = lat_u.indexer.shape;
+      auto fluid_v = InterpVelocity(lat_u, part_r[iPart]);
+      
+      for (int d=0; d<DQ_d; d++) {
+	part_v[iPart][d] = fluid_v[d];
+	part_r[iPart][d] += fluid_v[d];
+	/* deal with PBC */
+	part_r[iPart][d] = fmod(part_r[iPart][d] + size[d], double(size[d]));
+	/* and then update the unwrapped coords */
+	part_s[iPart][d] += fluid_v[d];
+      }
+    }
   }
 }
 
 void TracerArray::Move(Lattice* lat) {
-  const int numBlocks = (num + BlockSize - 1)/BlockSize;
-  
-  DoTracerArrayMove<<<numBlocks, BlockSize>>>(r.Device(), s.Device(), v.Device(),
-					      lat->data.u.Device());
+  targetLaunch(DoTracerArrayMove, r.Host().Shape())(r.Device(), s.Device(), v.Device(),
+						    lat->data.u.Device());
 }
