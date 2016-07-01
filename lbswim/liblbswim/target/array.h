710c47156cd82d7eeeb5b6cf26cad6d3f0004f32
Rupert Nash <rupert.nash@ed.ac.uk>
2016-07-01 14:59:49 +0100

    improve target array docs

--- lbswim/liblbswim/target/array.h
+++ lbswim/liblbswim/target/array.h
@@ -5,8 +5,9 @@
 #include <cstdlib>
 #include "func_attr.h"
 
-/* Minimal CUDA-compatible std::array replacement */
 namespace target {
+  
+  // Minimal CUDA-compatible std::array replacement
   template <typename T, size_t N>
   class array
   {
@@ -24,26 +25,38 @@ namespace target {
     __targetBoth__ static constexpr size_t size() {
       return N;
     }
-  
+
+    // Default constructor - contained type must also be default
+    // constructable
     __targetBoth__ array() {
       for (size_t i = 0; i<N; ++i)
 	data[i] = T();
     }
-  
+
+    // Fill constructor
+    __targetBoth__ array(const T& val) {
+      for (size_t i = 0; i<N; ++i)
+	data[i] = val;
+    }
+    
+    // Construct from N items
     template<typename... ARGS>
     __targetBoth__ array(ARGS... args) {
       static_assert(sizeof...(args) == N, "Wrong number of arguments");
       Init(0, args...);
     }
-    
+
+    // Element access
     __targetBoth__ T& operator[](size_t i) {
       return data[i];
     }
+    // Const element access
     __targetBoth__ const T& operator[](size_t i) const {
       return data[i];
     }
   };
 
+  // Equality operator - all elements must compare equal
   template <typename T, size_t N>
   __targetBoth__ bool operator==(const array<T,N>& a, const array<T,N>& b) {
 
